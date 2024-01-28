05df14ad0bb3744f5da08a3317810c5514ea57a4
Mohit Saud <mohit@prabidhilabs.com>
2024-01-28 23:06:37 +0545

    Implement route for get single activity
    This commit adds a new route handler for the GET /api/v1/activities /:id endpoint, providing functionality to retrieve an activity

--- src/routes/activityRoutes.ts
+++ src/routes/activityRoutes.ts
@@ -2,11 +2,13 @@ import express, { Router } from 'express';
 
 import {
   addActivity,
+  getActivity,
   getAllActivities,
 } from '../controllers/activityController';
 
 const router: Router = express.Router();
 
 router.route('/').post(addActivity).get(getAllActivities);
+router.route('/:id').get(getActivity);
 
 export default router;
