a329a7984b082111c512492de12fa339f28bd86b
Mohit Saud <mohit@prabidhilabs.com>
2024-01-28 16:21:57 +0545

    Setup db connection

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,11 @@
 import dotenv from 'dotenv';
-
+import 'express-async-errors';
 import express from 'express';
+
+import activityRouter from './routes/activityRoutes';
+import connectDB from './db/connectDB';
+import notFoundMiddleware from './middleware/not-found';
+import errorHandlerMiddleware from './middleware/error-handler';
 
 const app = express();
 
@@ -10,6 +15,27 @@ const PORT = process.env.PORT || 8000;
 
 app.use(express.json());
 
-app.listen(8000, () =>
-  console.log(`The server is listening on the port ${PORT}`)
-);
+// Mount the routers
+app.use('/api/v1/activities', activityRouter);
+
+// Middlewares
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const MONGO_URI: string | undefined = process.env.MONGO_URI;
+
+const start = async () => {
+  if (!MONGO_URI) {
+    console.error('MONGO_URI is not defined in the environment variables');
+
+    return;
+  }
+
+  await connectDB(MONGO_URI);
+
+  app.listen(PORT, () =>
+    console.log(`The server is listening on the port ${PORT}`)
+  );
+};
+
+start();
