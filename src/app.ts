0bf643f582a78cee7efec55e836dfa4444d8615e
Mohit Saud <mohit@prabidhilabs.com>
2024-01-28 12:22:58 +0545

    Add :: express.json() middleware

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,8 @@ const app = express();
 
 const PORT = process.env.PORT || 8000;
 
+app.use(express.json());
+
 app.listen(8000, () =>
   console.log(`The server is listening on the port ${PORT}`)
 );
