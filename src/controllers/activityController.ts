6ce1e414a8f242c61a592ff92bcaf5a6fb65a01f
Mohit Saud <mohit@prabidhilabs.com>
2024-01-28 22:00:21 +0545

    Get All Activities - Sort by -createdAt, Convert createdAt to localDate ("Asia/Kathmandu")

--- src/controllers/activityController.ts
+++ src/controllers/activityController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
+import moment from 'moment-timezone';
 
 import Activity from '../models/Activity';
 
@@ -12,11 +13,35 @@ export const addActivity = async (req: Request, res: Response) => {
   res.status(StatusCodes.CREATED).json({ activity });
 };
 
-// @desc Add Activity
+// @desc Get All Activities
 // @route GET /api/v1/activities
 // @access Public
 export const getAllActivities = async (req: Request, res: Response) => {
-  const activities = await Activity.find({});
+  const count = await Activity.countDocuments({});
 
-  res.status(StatusCodes.CREATED).json({ activities });
+  const allActivities = await Activity.aggregate([
+    {
+      $sort: { createdAt: -1 },
+    },
+    {
+      $addFields: {
+        date: {
+          $dateToString: {
+            format: '%Y-%m-%d %H:%M:%S',
+            date: '$createdAt',
+            timezone: 'Asia/Kathmandu',
+          },
+        },
+      },
+    },
+    {
+      $project: {
+        createdAt: 0,
+        updatedAt: 0,
+        __v: 0,
+      },
+    },
+  ]);
+
+  res.status(StatusCodes.OK).json({ count, allActivities });
 };
