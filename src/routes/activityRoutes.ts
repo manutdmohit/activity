dff77bc33f27120c5749da294e0136c5cb312d69
Mohit Saud <mohit@prabidhilabs.com>
2024-01-29 13:39:38 +0545

    Add Update Activity Route
    - Uses the PATCH method to update activity.
    - Can update description.
    - route: PATCH /api/v1/activities/:id

--- src/routes/activityRoutes.ts
+++ src/routes/activityRoutes.ts
@@ -4,11 +4,12 @@ import {
   addActivity,
   getActivity,
   getAllActivities,
+  updateActivity,
 } from '../controllers/activityController';
 
 const router: Router = express.Router();
 
 router.route('/').post(addActivity).get(getAllActivities);
-router.route('/:id').get(getActivity);
+router.route('/:id').get(getActivity).patch(updateActivity);
 
 export default router;
